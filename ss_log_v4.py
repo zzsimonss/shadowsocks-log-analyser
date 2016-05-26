1966acd762960804ddc5e669afba29be28f39d74
Andrew Zhang <zzhangshuaii@qq.com>
2016-05-26 21:24:05 +0800

    Create ss_log_v4.py

---
+++ ss_log_v4.py
@@ -0,0 +1,27 @@
+# Open the log file
+f=open('d:\shadowsocks.log')
+
+# initial a blank set
+s={1}
+s.remove(1)
+
+# begin to read log
+for line in f:
+
+	# find ip address start location,ip v4 address begin with '::ffff:'
+	loc = line.find('from ');
+	if  loc != -1:
+
+		# if find ip,then find the ':' before the port number,find from the loc+8
+		loc_p = line.find(':',loc+6)
+
+		# cut out ip form the line
+		ip = line[loc+5:loc_p]
+
+		# if ip not in the set
+		if ip not in s:
+			print(ip)
+
+			# add ip to the set
+			s.add(ip)
+f.close()
