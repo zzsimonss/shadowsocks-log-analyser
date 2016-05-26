771cefc275a34429a64e1b6eafddb46f000f154c
Andrew Zhang <zzhangshuaii@qq.com>
2016-05-26 22:27:53 +0800

    add times counting

--- ss_log_v6.py
+++ ss_log_v6.py
@@ -1,14 +1,15 @@
-# Open the log file
-f=open('shadowsocks.log')
+import operator
 
-# initial a blank set
-s={1}
-s.remove(1)
+# Open the log file
+f=open('d:\shadowsocks.log')
+
+# dic to store the ip
+s={}
 
 # begin to read log
 for line in f:
 
-	# find ip address start location,ip v4 address begin with '::ffff:'
+	# find ip address start location,ipv6 address begin with '::ffff:'
 	loc = line.find('::ffff:');
 	if  loc != -1:
 
@@ -18,10 +19,19 @@ for line in f:
 		# cut out ip form the line
 		ip = line[loc+7:loc_p]
 
-		# if ip not in the set
+		# if ip not in the dic
 		if ip not in s:
-			print(ip)
-
 			# add ip to the set
-			s.add(ip)
+			s.update({ip:1})
+		# if ip in the dic
+		else:
+			s[ip]+=1
+
+for w in sorted(s, key=s.get, reverse=True):
+  # Uncomment to print ips with its connected times
+  print(w+'		'+str(s[w]))
+
+  # Just print ips
+  #print(w)
+
 f.close()
