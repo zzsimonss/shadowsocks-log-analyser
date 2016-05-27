c38fca3282d5e915718421827f64dc055143a0b9
Andrew Zhang <zzhangshuaii@qq.com>
2016-05-27 11:41:18 +0800

    Create ss_log.py

---
+++ ss_log.py
@@ -0,0 +1,50 @@
+ipv6=False
+sort=False
+
+import operator
+
+# Open the log file
+f=open('d:\shadowsocks.log')
+
+# dic to store the ip
+s={}
+
+# begin to read log
+for line in f:
+
+	# find ip address start location,ipv6 address begin with '::ffff:'
+	loc = line.find('from ')
+	if ipv6:
+		loc = line.find('::ffff:')
+		
+	if  loc != -1:
+
+		# if find ip,then find the ':' before the port number,find from the loc+8
+		loc_p = line.find(':',loc+6)
+		if ipv6:
+			loc_p = line.find(':',loc+8)
+
+		# cut out ip form the line
+		ip = line[loc+5:loc_p]
+		if ipv6:
+			ip = line[loc+7:loc_p]
+		
+		# if ip not in the dic
+		if ip not in s:
+			# add ip to the set
+			s.update({ip:1})
+		# if ip in the dic
+		else:
+			s[ip]+=1
+if sort:
+	for w in sorted(s, key=s.get, reverse=True):
+	  # Uncomment to print ips with its connected times
+	  print(w+'		'+str(s[w]))
+
+	  # Just print ips
+	  #print(w)
+else:
+	for w in s:
+  		print(w+'     '+str(s[w]))
+
+f.close()
